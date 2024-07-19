afd7aad9b705de6ccf93fb69a986462539e5b4de
程序 <68020922+54chenxu@users.noreply.github.com>
2024-07-19 20:17:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -808,7 +808,7 @@ const getNormalConfigs = async (env, hostName, client) => {
                     ? `&security=tls&sni=${
                         randomUpperCase(hostName)
                     }&fp=randomized&alpn=${
-                        //client === 'singbox' ? 'http/1.1' : 'h2,http/1.1'
+                        // client === 'singbox' ? 'http/1.1' : 'h2,http/1.1' 2024.7.19 让ios上的Shadowrocket的ALN配置参数修改为h3
 			client === 'shadowrocket' ? 'h3' : 'h2,http/1.1'
                     }`
                     : ''}&path=${`/${getRandomPath(16)}${proxyIP ? `/${encodeURIComponent(btoa(proxyIP))}` : ''}`}${
