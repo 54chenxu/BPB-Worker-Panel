c83fd8e849f052466c122f0e1535f42b509297af
程序 <68020922+54chenxu@users.noreply.github.com>
2024-07-19 21:28:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -808,7 +808,7 @@ const getNormalConfigs = async (env, hostName, client) => {
                     ? `&security=tls&sni=${
                         randomUpperCase(hostName)
                     }&fp=randomized&alpn=${
-			client === 'shadowrocket' ? 'h3' : 'h2,http/1.1'
+			client === 'singbox' ? 'http/1.1' : 'h3' 
                     }`
                     : ''}&path=${`/${getRandomPath(16)}${proxyIP ? `/${encodeURIComponent(btoa(proxyIP))}` : ''}`}${
                         client === 'singbox' 
